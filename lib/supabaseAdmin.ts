3a215d38f659a6711238cbab9abb924e97583aa4
potato930922 <potato930922@gmail.com>
2025-09-20 19:01:51 +0900

    Update supabaseAdmin.ts

--- lib/supabaseAdmin.ts
+++ lib/supabaseAdmin.ts
@@ -2,11 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const url = process.env.SUPABASE_URL!;
-const key = process.env.SUPABASE_SERVICE_ROLE_KEY!; // 서비스 롤 키
-
-if (!url || !key) {
-  throw new Error('Missing SUPABASE_URL or SUPABASE_SERVICE_ROLE_KEY');
-}
+const key = process.env.SUPABASE_SERVICE_ROLE!;
 
 export const supabaseAdmin = createClient(url, key, {
   auth: { persistSession: false },
